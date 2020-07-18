5a41d0f721fda67b7c167884addc6fad612cb910
seanzhan0319 <seanzhan0319@hotmail.com>
2020-07-17 22:12:59 -0400

    Added more modules to buildexpt

--- src/components/Study/BuildExpt.js
+++ src/components/Study/BuildExpt.js
@@ -7,13 +7,69 @@ class BuildExpt extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      exptName: '',
+      numOfItems: Number,
+      showItems: false
     }
+
+    this.onChange = this.onChange.bind(this);
+    this.preBuild = this.preBuild.bind(this);
+    this.showItem = this.showItem.bind(this);
+    this.toggleShowItems = this.toggleShowItems.bind(this);
+    this.button = this.button.bind(this);
   }
 
-  inputExptName() {
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  item() {
     return (
       <div className="container">
+        Hello
+      </div>
+    )
+  }
+
+  showItem() {
+    console.log(this.state.numOfItems);
+    return (
+      <div>
+        {/* {Array(this.state.numOfItems).fill(
+          <div>
+            Hello
+          </div>
+        )} */}
+        {this.state.numOfItems}
+      </div>
+    )
+  }
+
+  toggleShowItems() {
+    this.setState({ showItems: true })
+  }
+
+  button() {
+    return (
+      <div>
+        <button class="btn" onClick={this.toggleShowItems}>Ok</button>
+      </div>
+    )
+  }
+
+  preBuild() {
+    return (
+      <div className="container">
+        Enter experiment name: <br/><br/>
+        <input type="text" name="exptName" value={this.state.exptName}
+          onChange={this.onChange}/> <br/><br/>
+        How many items do you want to build? <br/><br/>
+        <input type="text" name="numOfItems" value={this.state.numOfItems}
+          onChange={this.onChange}/> <p></p>
+          {
+            !this.state.showItems && 
+            <this.button/>
+          }
         
       </div>
     )
@@ -22,8 +78,10 @@ class BuildExpt extends Component {
   render() {
     return (
       <div className="container">
-        <br/>
-        Hello
+        <h3>Experiment Builder</h3> <br/>
+        {this.preBuild()} <br/>
+        {/* {this.showItems()} */}
+        { this.state.showItems && <this.showItem/>}
       </div>
     )
   }
